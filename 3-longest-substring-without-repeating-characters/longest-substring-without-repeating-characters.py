ac7c346299cc2980e28831831a72171bf897bbb7
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-04-29 18:33:17 +0530

    Time: 10 ms (80.72%) | Memory: 19.3 MB (69.44%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,19 +1,20 @@
-class Solution:
-    def lengthOfLongestSubstring(self, s): 
-        storage = set()
-        left = 0
-        max_length = 0
+# print the longest substring without repeating characters 
+class Solution(): 
+	def lengthOfLongestSubstring(self, s):
+		storage = set()
+		left = 0 # left starts with the 0th point and the right is looped
+		max_length = 0 #this is something we are going to test at each step
 
-        for right in range(len(s)): 
-            while s[right] in storage: 
-                storage.remove(s[left])
-                left += 1
-
-            storage.add(s[right])
-            max_length = max(max_length, right - left + 1)
-
-        return max_length
+		for right in range(len(s)): 
+			while s[right] in storage: 
+				storage.remove(s[left]) # remove the value of the left var		
+				left += 1
+			storage.add(s[right])
+			max_length = max(max_length, right - left + 1)
+		
+		return max_length
 
 string = "abcabcbb"
 solution = Solution()
 print(solution.lengthOfLongestSubstring(string))
+		

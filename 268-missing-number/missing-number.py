2b84929624a93aa5e80dbbbd7447cddc08c0f5ab
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-04-29 20:41:45 +0530

    Time: 3 ms (59.68%) | Memory: 20.5 MB (42.12%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -2,6 +2,8 @@ class Solution:
     def missingNumber(self, nums): 
         n = len(nums) + 1 
         expected_sum = n * (n - 1) // 2 
+        # this is where we get the actual sum this is the formula 
+        # "//" is done to get integer value and if we did "/" then return woould have been a float number
         actual_sum = sum(nums)
         return expected_sum - actual_sum
 

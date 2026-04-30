33b8b85358ca27b0c02ed8a7370a41c28262458b
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-04-30 20:15:11 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (88.55%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -4,7 +4,9 @@ class Solution:
         for right in range(len(nums)): 
             if nums[right] != val: 
                 nums[left] = nums[right]
+                # this line is to change the value on the list this is doing nothing to the index
                 left += 1
+                # this line is to change the index, increment by +1
         return left 
 
 nums = [0,1,2,2,3,0,4,2]
@@ -12,4 +14,5 @@ val = 2
 solution = Solution()
 length = solution.removeElement(nums, val)
 print(f"the new length of the array is : {length},                                                              the new list contains {nums[:length]} these elements")
-# here we are ending just before the duplication of the elements starts
+# here we are ending just before the duplication of the elements starts 
+# because we are not actually changing the length of the array we are removing the elements that we do nto need and then just printing the changed poortion according to our needs 

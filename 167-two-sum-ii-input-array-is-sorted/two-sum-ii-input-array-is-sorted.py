83eedd73c1e88f102e9558ebfe5083fac4dd8458
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-04-30 19:39:25 +0530

    Time: 3 ms (79.77%) | Memory: 20.6 MB (7.64%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -6,6 +6,7 @@ class Solution:
             current_sum = numbers[left] + numbers[right]
             if current_sum == target: 
                 return [left+1, right+1]
+                # we are returning the index values here and then adding one to it to get the actuak value because the values in the list starts with 1 and the index starts with 0 so thats why if we add one to it they are same as the number because the list is already sorted
             elif current_sum < target: 
                 left += 1
             else: # current_sum > target

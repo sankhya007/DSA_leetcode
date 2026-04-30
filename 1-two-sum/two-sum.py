c2503af7071217ac5bbb514a9a4acd5e632d71cf
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-04-30 11:43:16 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (43.20%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,11 +1,13 @@
 class Solution:
     def twoSum(self, nums, target): 
-        num_set = {}
-        for i, num in enumerate(nums):
+        num_set = {} 
+        # because we are storing the value and the index thats why used a set
+        for i, num in enumerate(nums): #enumerate because it turns it into _:_
             complement = target - num
             if complement in num_set: 
-                return [num_set[complement], i]
-            num_set[num] = i
+                return [num_set[complement], i] #getting both of the index values
+            num_set[num] = i #this guy is just adding the values in the numset
+            #num_set[2] = 0 so we add {2:0} adn goes on tll the list ends
 
 nums = [2,7,11,15]
 target = 9

b6afd62eb4064c792e872ded16cc4d3f1defc39c
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-05-01 12:05:03 +0530

    Time: 22 ms (59.49%) | Memory: 25.6 MB (43.85%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -0,0 +1,16 @@
+class Solution:
+    def productExceptSelf(self, nums): 
+        length = len(nums)
+        answer = [1]*length
+        left_product = 1
+        for i in range(length): 
+            answer[i] *= left_product
+            left_product *= nums[i]
+        right_product = 1
+        for i in range(length - 1, -1, -1):
+            answer[i] *= right_product
+            right_product *= nums[i]
+        return answer
+nums = [1,2,3,4,5]
+solution = Solution()
+print(solution.productExceptSelf(nums))

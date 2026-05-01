5038f983a91af2d85231eb5eb47d87b4ab87d228
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-05-01 11:30:02 +0530

    Time: 25 ms (91.64%) | Memory: 28.6 MB (76.61%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,13 +1,16 @@
 class Solution:
     def maxProfit(self, prices): 
-        min_price = float("inf")
-        max_profit = 0 
+        min_price = float("inf") # every number will be lower than this thats why infinity
+        max_profit = 0 # every number will be bigger than this because then how can it be profit if it is in negative thats why it is zero 
+
         for price in prices: 
             if price < min_price: 
                 min_price = price
             elif price - min_price > max_profit: 
                 max_profit = price - min_price
+
         return max_profit
+        
 prices = [7,1,5,3,6,4]
 solution = Solution()
 print(solution.maxProfit(prices))

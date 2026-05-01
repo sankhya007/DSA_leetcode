e4a9d3d934fa2ead29fb1e71f7034f05185aaa7f
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-05-01 20:41:05 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (10.23%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -5,23 +5,30 @@ class Solution:
         
         while left <= right: 
             mid = left + (right - left) // 2
+            #again this iss the only way to get the mid
 
             if nums[mid] == target: 
                 return mid 
+            # if the target is mid, to get the final answer
 
             elif nums[left] <= nums[mid]: 
+            # if the target is in the 1st half 
                 if nums[left] <= target < nums[mid]: 
                     right = mid - 1
-                else: 
+                else:
+                # if the search fail for the 1st half
                     left = mid + 1
 
-            else: 
+            else:
+            # if the target is in the 2nd half 
                 if nums[mid] < target <= nums[right]: 
                     left = mid + 1
                 else: 
+                # if the search faiil for the 2nd half meaning this is in the 1st half
                     right = mid -1
         
         return -1 
+        # if you do not find the target return -1, as result not found
 
 nums = [4,5,6,7,0,1,2]
 target = 0

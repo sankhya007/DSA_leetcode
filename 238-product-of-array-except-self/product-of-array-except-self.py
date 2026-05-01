11a5074a478f50f6eed27c2dd59e7fce07937147
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-05-01 12:08:01 +0530

    Time: 22 ms (59.49%) | Memory: 25.5 MB (54.72%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -2,15 +2,21 @@ class Solution:
     def productExceptSelf(self, nums): 
         length = len(nums)
         answer = [1]*length
+        # this is helping us create a list that is similler length as the given list but only contains '1'
+
         left_product = 1
         for i in range(length): 
-            answer[i] *= left_product
-            left_product *= nums[i]
+            answer[i] *= left_product # answer gets multiplied with the left_product
+            left_product *= nums[i] # left_product gets multiplied with the nums list in the similler index
+
         right_product = 1
         for i in range(length - 1, -1, -1):
-            answer[i] *= right_product
-            right_product *= nums[i]
+            # this is going in the opposite direction starting from the end and to the 1sst
+            answer[i] *= right_product # answer gets multiplied with the right_product
+            right_product *= nums[i] # right_product gets multiplied with the nums list in the similler index
+
         return answer
+        
 nums = [1,2,3,4,5]
 solution = Solution()
 print(solution.productExceptSelf(nums))

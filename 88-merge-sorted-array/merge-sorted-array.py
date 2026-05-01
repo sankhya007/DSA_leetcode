9897e7a5c0443b66e973812c3856ef10fd296ee3
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-05-01 07:05:59 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (77.24%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -2,7 +2,7 @@ class Solution:
     def merge(self, nums1, m, nums2, n): 
         p1 = m - 1
         p2 = n - 1
-        p = m + n - 1
+        p = m + n - 1 # thsi is done to get the actual index value of the lists that we have
         
         while p1 >= 0 and p2 >= 0: 
             if nums1[p1] > nums2[p2]: 
@@ -13,12 +13,14 @@ class Solution:
                 p2 -= 1
             p -= 1
 
-        while p2 >= 0: 
+        while p2 >= 0: # this is so there are no left over values in the p2 or nume nums2 because if there are more elements in the p2 than the p2 then the p1 is going to end fast thats why this is done 
             nums1[p] = nums2[p2]
             p2 -= 1
             p -= 1
+    
+        # and we do not have to do the same for the p1 because we are storing all the values in the already existing p1 and tahst wwhy if teh p2 ends before p1 then we would just let them stay there
 
-        return nums1
+        return nums1 # coz we are storing all the values in nums1
 
 nums1 = [1,2,3,0,0,0]
 m = 3

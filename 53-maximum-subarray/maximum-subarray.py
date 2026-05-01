b57563386bdf982172c3a5f359d3acd4691bd89f
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-05-01 07:48:04 +0530

    Time: 37 ms (53.37%) | Memory: 31.6 MB (20.41%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -1,11 +1,17 @@
 class Solution:
     def maxSubArray(self, nums):
         max_current = max_global = nums[0]
+        # setting both of the values to the 1st element of the array 
+
         for i in range(1, len(nums)): 
             max_current = max(nums[i], max_current + nums[i])
+            # find the maxx form the already existing sum and the adition of the next element
             if max_current > max_global: 
-                max_global = max_current
+                max_global = max_current 
+                # overwrite the global element if the current element is bigger than it
+
         return max_global
+        
 nums = [-2,1,-3,4,-1,2,1,-5,4]
 solution = Solution()
 print(solution.maxSubArray(nums))

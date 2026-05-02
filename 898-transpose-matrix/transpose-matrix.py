09dfdf7b2e1671f52a0baf691d78a9953ac023f8
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-05-02 15:38:14 +0530

    Time: 0 ms (100.00%) | Memory: 19.9 MB (16.03%) - LeetSync

--- 898-transpose-matrix/transpose-matrix.py
+++ 898-transpose-matrix/transpose-matrix.py
@@ -1,13 +1,13 @@
 class Solution:
     def transpose(self, matrix): 
         rows = len(matrix)
-        cols = len(matrix[0])
+        cols = len(matrix[0]) # this is to get the length of the 2d array we are saying a matrix so the number of elemets in the array will be the length of the rows and the number of elements inside the elemts will be the columns
 
-        transposed = [[0] * rows for _ in range(cols)]
+        transposed = [[0] * rows for _ in range(cols)] # this is to intercgange the length of the rows and the columns
 
         for i in range(rows): 
             for j in range(cols):
-                transposed[j][i] = matrix[i][j] 
+                transposed[j][i] = matrix[i][j] # just adding the values in the existing matrix(transposed)
 
         return transposed
 

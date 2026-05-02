4abaf73e64f683a860a6a41ab865512eb58fac47
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-05-02 06:45:40 +0530

    Time: 31 ms (77.70%) | Memory: 22 MB (28.92%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,14 +1,18 @@
 class Solution:
     def subarraySum(self, nums, k): 
         count = 0 
-        current_sum = 0
-        sub_array = {0:1}
+        current_sum = 0 # giving empty containers to store teh value 
+        sub_array = {0:1} # here here we are creating the set with 0 being seen already once 
+
         for num in nums: 
-            current_sum += num
-            if current_sum - k in sub_array: 
-                count += sub_array[current_sum - k]
-            sub_array[current_sum] = sub_array.get(current_sum , 0) + 1
+            current_sum += num # addding the values of numes one by one 
+            # current_sum - k = the numerber that is left to get to k
+            if current_sum - k in sub_array: # does it exist in the sup array
+                count += sub_array[current_sum - k] # gets how ever many times it has appreared or however is it's count already is and adds to that
+            sub_array[current_sum] = sub_array.get(current_sum , 0) + 1 # this is to add the var in the set, it it already ecist then just add it directly and increase index by 1 and of not then just create it 
+            
         return count
+
 nums = [1,1,1]
 k = 2
 solution = Solution()

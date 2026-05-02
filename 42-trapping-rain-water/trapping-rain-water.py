46e71bca095598c2a624db5a6204c0a0bd6d2149
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-05-02 12:03:31 +0530

    Time: 3 ms (95.20%) | Memory: 21.2 MB (11.92%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -2,25 +2,32 @@ class Solution:
     def trap(self, height):
         if not height:
             return 0
+        # this is so if the list if empty the return is 0(no water trapped)
+
         left = 0
         right = len(height) - 1
         max_left = 0
         max_right = 0
-        trapped_water = 0
+        trapped_water = 0 # all the variable containters
+
         while left < right:
             if height[left] < height[right]:
                 if height[left] >= max_left:
-                    max_left = height[left]
+                    max_left = height[left] # to get the max height of the left wall we draw this equation
                 else:
-                    trapped_water += max_left - height[left]
+                    trapped_water += max_left - height[left] # this is done in one block so thats why it looks like thuis its collects both of the side of a single piller and then does the calculation
                 left += 1
-            else:
+
+            else: # height[right] < height[left]
                 if height[right] >= max_right:
-                    max_right = height[right]
+                    max_right = height[right] # to get the max right wall height
                 else:
-                    trapped_water += max_right - height[right]
+                    trapped_water += max_right - height[right] # also calculating one block similler to the previous block's calculation
                 right -= 1
+
         return trapped_water
+
+        
 height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
 solution = Solution()
 print(solution.trap(height))

1bb2b3be2290f744befc8a3a6b92dfc5141fd463
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-05-02 22:58:24 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (13.70%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,22 +1,22 @@
 class Solution:
     def searchMatrix(self, matrix, target): 
-        if not matrix: 
+        if not matrix: # if matrix empty
             return False
         
         rows = len(matrix)
-        cols = len(matrix[0])
+        cols = len(matrix[0]) #initialize vals
         left = 0 
-        right = rows * cols - 1
+        right = rows * cols - 1 # the last element in 2d array 
     
         while left <= right: 
-            mid = left + (right - left) // 2
-            mid_value = matrix[mid // cols][mid % cols]
+            mid = left + (right - left) // 2 # this is how it is 
+            mid_value = matrix[mid // cols][mid % cols] # this is also how it is 
 
-            if mid_value == target: 
+            if mid_value == target: # fkn easy asf logic
                 return True
             elif mid_value < target: 
                 left = mid + 1
-            else: 
+            else: # mid_value > target
                 right = mid - 1
         
         return False

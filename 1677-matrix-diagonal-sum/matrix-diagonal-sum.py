8e1a5e433c9fc8ab48fe7da388a36ce88b87bd58
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-05-03 06:16:17 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (54.25%) - LeetSync

---
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.py
@@ -0,0 +1,19 @@
+class Solution:
+    def diagonalSum(self, mat): 
+        n = len(mat)
+        total_sum = 0
+
+        for i in range(n):
+            total_sum += mat[i][i]
+            if i != n - 1 - i:
+                total_sum += mat[i][n - 1 - i]
+
+        return total_sum
+
+mat = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+    ]
+solution = Solution()
+print(solution.diagonalSum(mat))

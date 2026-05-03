d1869a0ae7fe021e7d9bc26495194625dba8dd64
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-05-03 07:42:17 +0530

    Time: 55 ms (49.20%) | Memory: 22.5 MB (86.10%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.py
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -13,8 +13,15 @@ class Solution:
             slow = slow.next
             fast = fast.next.next 
 
-            if slow == fast: 
+            if slow == fast: # if at any point of the code the slow and the fast pointer is the same that means it has a loop in the linked lsit
                 return True
             
         return False 
 
+    # 1 → 2 → 3 → 4 → 5
+    #         ↑       ↓
+    #          ← ← ← ←
+    # when fast reaches 5 the slow is at 3 
+    # now because of the loop the fast is going to move to 4 ( 5 - 3 - 4 # double skip)
+    # and the slow is going to move to 4 ( 3 - 4 # single skip)
+    # both are at the same node, means there is a loop in the linked list

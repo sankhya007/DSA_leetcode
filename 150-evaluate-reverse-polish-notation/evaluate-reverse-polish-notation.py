bc6fe577416aa9dc74646dd3a65bfffa117ff2e0
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-05-04 09:33:31 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (93.56%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -11,9 +11,11 @@ class Solution:
                 if token == '+': stack.append(a + b)
                 elif token == '-': stack.append(a - b)
                 elif token == '*': stack.append(a * b)
-                elif token == '/': stack.append(int(a / b))
+                elif token == '/': stack.append(int(a / b)) # trunkate towards zero 
+                # you can not use "//" / round down function because 5 // 2 = 3 but int(5/2) = 2 closer to zero
 
             else: 
                 stack.append(int(token))
 
         return stack[0]
+        # after doing all that the stack is going to contain only one value but of we use return stack we are going to get a list in return like "[9]" and that is not what we want, we want a value like 9 that's why we are calling an index 

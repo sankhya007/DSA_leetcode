09f28f3b042e53673ccb660426784382dcdb7d9b
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-05-05 12:14:53 +0530

    Time: 105 ms (93.43%) | Memory: 31 MB (79.44%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -2,13 +2,13 @@ class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
         stack = []
         max_area = 0
-        heights.append(0)
+        heights.append(0) # this is just a dummy element that we are adding
 
-        for i, h in enumerate(heights): 
-            while stack and heights[stack[-1]] > h: 
-                height = heights[stack.pop()]
-                width = i if not stack else i - stack[-1] - 1
-                max_area = max(max_area, height * width)
-            stack.append(i)
+        for i, h in enumerate(heights): # turn them into two sets 
+            while stack and heights[stack[-1]] > h: # if the stack has some elements compare the top element of the stack with thewith the recent height with the loop element 
+                height = heights[stack.pop()] # pop the top element of the stack as height 
+                width = i if not stack else i - stack[-1] - 1 # i if the stack is empty and if there is a value in the stack then so the following 
+                max_area = max(max_area, height * width) # get the max 
+            stack.append(i) # append every element 
 
-        return max_area
+        return max_area 

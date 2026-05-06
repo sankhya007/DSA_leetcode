460927f787e336b96ea3ff0e7f268fa5aa0321e3
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-05-06 07:41:59 +0530

    Time: 119 ms (69.98%) | Memory: 52.1 MB (91.48%) - LeetSync

--- 1972-rotating-the-box/rotating-the-box.py
+++ 1972-rotating-the-box/rotating-the-box.py
@@ -4,21 +4,21 @@ class Solution:
         n = len(boxGrid[0])
 
         for row in boxGrid: 
-            empty = n - 1 # right most position of the row
+            empty = n - 1 # right most position of the row so when we are goin to rotate that 90 degrees this is going to be the bottom of the box and the index of the bottom of the box is this 
 
             for j in range(n - 1, -1, -1): 
                 if row[j] == '*': 
-                    empty = j - 1
+                    empty = j - 1 # because the stone can not pass the obstacle that is why we are going decrease the length of the end of the box 
                 elif row[j] == '#': 
                     row[j] = '.'
                     row[empty] = '#'
-                    empty -= 1
+                    empty -= 1 # stone comes down , the position becomes empty and then the empty space index decreses by 1
                 
-        res = [[None] * m for _ in range(n)]
+        res = [[None] * m for _ in range(n)] # making a clear array with the rows and cols inversed as the standerd one 
 
         for i in range(m): 
             for j in range(n): 
-                res[j][m - 1 - i] = boxGrid[i][j]
+                res[j][m - 1 - i] = boxGrid[i][j] # placing the values from the box to the res , the changed values 
 
         return res
 

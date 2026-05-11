dc3aba3c829cee7f34ffc5a7e458318f7f92fee9
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-05-11 18:51:47 +0530

    Time: 3 ms (48.36%) | Memory: 23.5 MB (45.85%) - LeetSync

--- 344-reverse-string/reverse-string.py
+++ 344-reverse-string/reverse-string.py
@@ -2,13 +2,11 @@ class Solution():
     def reverseString(self, s): 
         left = 0
         right = len(s) - 1 
-        while left < right: 
+        while left < right:     
             s[left], s[right] = s[right], s[left]
             left += 1
             right -= 1
-        # checking of the sync works 
-
-s = "nigga"
-string = list(s)
-solution = Solution()
-print(solution.reverseString(string))
+s = "what the fuck is wrong with the job market"
+sentence = list(s)
+solutoin = Solution()
+print(solutoin.reverseString(sentence))

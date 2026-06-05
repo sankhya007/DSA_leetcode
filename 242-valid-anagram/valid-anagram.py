eee75dd61a53a02e11fdd3f4a162fe9f93eaf8e7
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-06-05 19:40:35 +0530

    Time: 15 ms (43.21%) | Memory: 20.3 MB (16.10%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,10 +1,3 @@
-# check for anagram 
-
-class Solution(): 
-	def isAnagram(self, s1, s2): 
-		return sorted(s1) == sorted(s2)
-
-string1 = "tea"
-string2 = "ate"
-solution = Solution()
-print(solution.isAnagram(string1, string2))
+class Solution():
+    def isAnagram(self, s1, s2):
+        return sorted(s1) == sorted(s2)

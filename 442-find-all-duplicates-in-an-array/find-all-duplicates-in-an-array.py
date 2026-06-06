4a395f64c744ce81aed22b70b74b551a417b815a
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-06-06 20:33:14 +0530

    Time: 24 ms (80.74%) | Memory: 32.4 MB (40.38%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.py
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.py
@@ -1,14 +1,10 @@
-class Solution:
-    def findDuplicates(self, nums):
-        num_set = set() # storing them in a set
-        duplicates = set() # making sure the duplicates list does not have any duplicates
+class Solution(): 
+    def findDuplicates(self, nums): 
+        num_set = set()
+        duplicates = set()
         for num in nums: 
             if num in num_set: 
                 duplicates.add(num)
-            else: # num not in num_set
+            else: 
                 num_set.add(num)
-        return list(duplicates) # turning them into a list from a set
-
-nums = [4,3,2,7,8,2,3,1]
-solution = Solution()
-print(solution.findDuplicates(nums))
+        return list(duplicates)

1e96ccc27d9c15a0d0be17eb189c66e4850b051d
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-06-06 20:37:49 +0530

    Time: 4 ms (6.15%) | Memory: 19.2 MB (77.66%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.py
+++ 2058-concatenation-of-array/concatenation-of-array.py
@@ -1,8 +1,3 @@
 class Solution:
     def getConcatenation(self, nums): 
         return nums + nums 
-        # what is the point of this again ??
-
-nums = [1,3,2,1]
-solution = Solution()
-print(solution.getConcatenation(nums))

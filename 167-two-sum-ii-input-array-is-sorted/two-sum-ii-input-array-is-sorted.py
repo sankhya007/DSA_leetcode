243438e33d0ef1f43c8266d22cccc79c3b3e379c
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-06-07 13:44:17 +0530

    Time: 7 ms (28.74%) | Memory: 20.6 MB (7.77%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,4 +1,4 @@
-class Solution:
+class Solution: 
     def twoSum(self, numbers, target): 
         left = 0 
         right = len(numbers) - 1
@@ -6,13 +6,8 @@ class Solution:
             current_sum = numbers[left] + numbers[right]
             if current_sum == target: 
                 return [left+1, right+1]
-                # we are returning the index values here and then adding one to it to get the actuak value because the values in the list starts with 1 and the index starts with 0 so thats why if we add one to it they are same as the number because the list is already sorted
             elif current_sum < target: 
                 left += 1
-            else: # current_sum > target
+            elif current_sum > target: 
                 right -= 1
         return None
-numbers = [1,2,3,4,5,6,7,8,9]
-target = 7
-solution = Solution()
-print(str(solution.twoSum(numbers, target)))

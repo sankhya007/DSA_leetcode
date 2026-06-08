07697eb0fc3944185b40f3f701b5dfadb2acf3e3
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-06-08 20:46:11 +0530

    Time: 3 ms (86.26%) | Memory: 21.1 MB (87.72%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,17 +1,9 @@
 class Solution:
-    def majorityElement(self, nums):
+    def majorityElement(self, nums): 
         count = 0
-        candidate = None 
-        # ths is None because if it was 0, it could have also been a candidate in the given list
-
+        candidate = None
         for num in nums: 
-            if count == 0: 
-                candidate = num #if the count hits 0 then the candidate can change or else the count just does down by 1
+            if count == 0:
+                candidate = num
             count += (1 if candidate == num else -1)
-            # if the number matches then the count goes up or else down
-
         return candidate
-
-nums = [2,2,1,1,1,2,2]
-solution = Solution()
-print(solution.majorityElement(nums))

2248c3a58282b25b4dd4ff4805e0222bbee18dfb
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-06-08 20:23:42 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (78.50%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -1,24 +1,10 @@
 class Solution:
     def removeDuplicates(self, nums): 
-
         if not nums: 
-            return "the list is empty"
-            # this is done in case the list if elements is empty
-
-        left = 0
-        for right in range(1, len(nums)):
-            # here we start from 1, or else the left and right will be the same number 
+            return "empty array"
+        left = 0 
+        for right in range(0, len(nums)): 
             if nums[left] != nums[right]: 
                 left += 1
-                # this is changing the index
                 nums[left] = nums[right]
-                # this is changing the value in the list
-
         return left + 1
-        # we increment the value by 1 because we need the length of the list and the left is the 2nd last value of the list so if we return than we could not get the actual length and thats why we return the value incremented by 1
-
-nums = [0,0,1,1,1,2,2,3,3,4]
-
-solution = Solution()
-new_length = solution.removeDuplicates(nums)
-print(f"the new length of the new list is : {new_length},                                                       and {nums[:new_length]} is the new list, without duplicates.")

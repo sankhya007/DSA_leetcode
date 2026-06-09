8caaf3e6c74d6e1bf7aace178d6ce67d456c9532
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-06-09 14:19:14 +0530

    Time: 57 ms (60.24%) | Memory: 29.6 MB (68.10%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,21 +1,14 @@
-class Solution:
-    def maxArea(self, height): 
+class Solution: 
+    def maxArea(self, heights): 
         left = 0 
-        right = len(height) - 1
-        max_area = 0 # container to store the max_area because that function is not getthing called inside the loop 
-
+        right = len(heights) - 1
+        max_area = 0
         while left < right: 
-            width = right - left # width is just a value we created just to do the multiplicaiton and that is dependent on the index because there is no other factor there is to multiply with the height
-            current_area = min(height[right], height[left]) * width 
+            width = right - left
+            current_area = min(heights[left], heights[right]) * width
             max_area = max(max_area, current_area)
-
-            if height[left] < height[right]: # here we need to use the values in the height because if not then we would be using the index and that's not right
+            if heights[left] < heights[right]: 
                 left += 1
-            else: 
+            else: # right < left 
                 right -= 1
-
         return max_area
-
-height = [1,8,6,2,5,4,8,3,7]
-solution = Solution()
-print(solution.maxArea(height))

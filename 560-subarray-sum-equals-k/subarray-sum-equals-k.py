fd15549d9d3a1c4d3d02dcccd4e4e5f02cf2c376
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-06-09 19:01:38 +0530

    Time: 29 ms (83.20%) | Memory: 21.7 MB (97.06%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,19 +1,11 @@
-class Solution:
-    def subarraySum(self, nums, k): 
-        count = 0 
-        current_sum = 0 # giving empty containers to store teh value 
-        sub_array = {0:1} # here here we are creating the set with 0 being seen already once 
-
+class Solution: 
+    def subarraySum(self, nums, k):
+        count = 0
+        current_sum = 0
+        prefix_sum = {0:1}
         for num in nums: 
-            current_sum += num # addding the values of numes one by one 
-            # current_sum - k = the numerber that is left to get to k
-            if current_sum - k in sub_array: # does it exist in the sup array
-                count += sub_array[current_sum - k] # gets how ever many times it has appreared or however is it's count already is and adds to that
-            sub_array[current_sum] = sub_array.get(current_sum , 0) + 1 # this is to add the var in the set, it it already ecist then just add it directly and increase index by 1 and of not then just create it 
-            
+            current_sum += num
+            if current_sum - k in prefix_sum: 
+                count += prefix_sum[current_sum - k]
+            prefix_sum[current_sum] = prefix_sum.get(current_sum, 0) + 1
         return count
-
-nums = [1,1,1]
-k = 2
-solution = Solution()
-print(solution.subarraySum(nums, k))

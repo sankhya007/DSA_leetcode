de7480afbce1f060778140067a5c4c08b002d247
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-06-09 19:49:58 +0530

    Time: 6 ms (59.52%) | Memory: 26.8 MB (19.29%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -1,19 +1,12 @@
-class Solution:
+class Solution(): 
     def rotate(self, nums, k): 
-        k = k % len(nums) # if teh value of the k is higher than the length of the list
-        self.reverse(nums, 0, len(nums) - 1) # reverse the whole array 
-        self.reverse(nums, 0, k - 1) # reverse till k'th index or the 1st half
-        self.reverse(nums, k, len(nums) - 1) # reverse the 2nd half of the reversed array
-
-    def reverse(self, nums, start, end): # helping use reverse
+        k = k % len(nums)
+        self.reverse(nums, 0, len(nums) - 1)
+        self.reverse(nums, 0, k - 1) #1st half
+        self.reverse(nums, k, len(nums) - 1) #2nd half
+    def reverse(self, nums, start, end): 
         while start < end: 
             nums[start], nums[end] = nums[end], nums[start]
             start += 1
             end -= 1
-
-nums = [1,2,3,4,5,6,7]
-k = 3
-
-solution = Solution()
-solution.rotate(nums, k)
-print(nums) # coz no return 
+    

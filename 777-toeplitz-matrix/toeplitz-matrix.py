e414d76c2e998d60107042cd756a2533770dec1d
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-06-09 22:32:18 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (23.80%) - LeetSync

--- 777-toeplitz-matrix/toeplitz-matrix.py
+++ 777-toeplitz-matrix/toeplitz-matrix.py
@@ -1,15 +1,9 @@
 class Solution:
-    def isToeplitzMatrix(self, matrix):
+    def isToeplitzMatrix(self, matrix): 
         rows = len(matrix)
         cols = len(matrix[0])
-
-        for i in range(rows - 1):
-            for j in range(cols - 1):
-                if matrix[i][j] != matrix[i + 1][j + 1]: # this is is the wway to get the next element in the same diagonal of the matrix so (0, 0) to (1, 1) if not same return False
+        for i in range(rows - 1): 
+            for j in range(cols - 1): 
+                if matrix[i][j] != matrix[i+1][j+1]:
                     return False
-
         return True
-
-matrix = [[1,2,3,4],[5,1,2,3],[9,5,1,2]]
-solution = Solution()
-print(solution.isToeplitzMatrix(matrix))

d2497bb2611f8fbfb5182595ce80020e834b51d8
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-06-13 19:54:16 +0530

    Time: 7 ms (59.81%) | Memory: 20.3 MB (36.39%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -1,23 +1,16 @@
-class Solution:
+class Solution: 
     def setZeroes(self, matrix): 
         rows = len(matrix)
-        cols = len(matrix[0]) # initialize the length
+        cols = len(matrix[0])
         zero_rows = set()
-        zero_cols = set() # here we are adding the storage for the location of the rows and cols so that they gat added in a set and there is no duplicate so then the loop do not have to go through unneeded steps 
-
-        for i in range(rows): # this step is done to identify the positions where we can see the 0 
-            for j in range(cols):
+        zero_cols = set()
+        for i in range(rows): 
+            for j in range(cols): 
                 if matrix[i][j] == 0: 
                     zero_rows.add(i)
                     zero_cols.add(j)
-
-        for i in range(rows): # this step is done to make all the vals thats we have in the cols and rows connected to that position zeros indivisually 
-            for j in range(cols):
-                if i in zero_rows or j in zero_cols: # here the "or" is really important 
+        for i in range(rows): 
+            for j in range(cols): 
+                if i in zero_rows or j in zero_cols: 
                     matrix[i][j] = 0
-
-        return matrix 
-
-matrix = [[1,1,1],[1,0,1],[1,1,1]]
-solution = Solution()
-print(solution.setZeroes(matrix))
+        return matrix

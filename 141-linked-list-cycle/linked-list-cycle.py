5dcab5d2bd80045348a32885ad05bde6ef2576e3
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-06-16 17:31:27 +0530

    Time: 51 ms (72.73%) | Memory: 22.5 MB (85.59%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.py
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -1,27 +1,10 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
-class Solution:
-    def hasCycle(self, head: Optional[ListNode]) -> bool:
-        slow = head 
-        fast = head 
-
-        while fast and fast.next:
+class Solution: 
+    def hasCycle(self, head): 
+        slow = head
+        fast = head
+        while fast and fast.next: 
+            fast = fast.next.next
             slow = slow.next
-            fast = fast.next.next 
-
-            if slow == fast: # if at any point of the code the slow and the fast pointer is the same that means it has a loop in the linked lsit
+            if slow == fast: 
                 return True
-            
-        return False 
-
-    # 1 → 2 → 3 → 4 → 5
-    #         ↑       ↓
-    #          ← ← ← ←
-    # when fast reaches 5 the slow is at 3 
-    # now because of the loop the fast is going to move to 4 ( 5 - 3 - 4 # double skip)
-    # and the slow is going to move to 4 ( 3 - 4 # single skip)
-    # both are at the same node, means there is a loop in the linked list
+        return False

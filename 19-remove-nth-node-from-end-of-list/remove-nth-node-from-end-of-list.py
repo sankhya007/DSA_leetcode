677ada1284387120d2d4a6b545f0b47bb975f5bf
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-06-16 18:18:05 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (90.82%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -1,22 +1,12 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution:
-    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        dummy = ListNode(0, head) # creating a node, val = 0 and the pointer is head 
-        slow = dummy 
-        fast = dummy # the slow andd the fast pointers are meant to point to the head but we created a dummy node which is not the head so they are pointing to the dummy 
-
-        for _ in range(n): # n is the index of the element we want to remove ,so we forward fast pointer by n steps
+class Solution: 
+    def removeNthFromEnd(self, head, n): 
+        dummy = ListNode(0, head)
+        slow = dummy
+        fast = dummy
+        for _ in range(n): 
             fast = fast.next
-        
-        while fast.next: # now we increment both by one step till there is no element left in the list
+        while fast.next: 
             slow = slow.next 
             fast = fast.next
-
-        slow.next = slow.next.next # skipping the element that we want to delete 
-
-        return dummy.next # skipping the head when we are printing the array, coz it was dummy 
-
+        slow.next = slow.next.next
+        return dummy.next

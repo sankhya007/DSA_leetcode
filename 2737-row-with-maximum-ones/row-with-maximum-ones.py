8c58859ffea5f7d68049b3f390c2128ecd4a0c3c
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-06-16 06:16:11 +0530

    Time: 7 ms (65.26%) | Memory: 20.1 MB (14.89%) - LeetSync

--- 2737-row-with-maximum-ones/row-with-maximum-ones.py
+++ 2737-row-with-maximum-ones/row-with-maximum-ones.py
@@ -1,18 +1,10 @@
-class Solution():
+class Solution: 
     def rowAndMaximumOnes(self, mat): 
         max_ones = 0 
-        row_index = 0 # this does not have to be -1 
-
-        for i in range(len(mat)):
-            current_ones = sum(mat[i]) # just gonna add all the values and because it is 1s and 0s ony the 1s are gonna add up and then the sum will be the total number of 1s 
-            if current_ones > max_ones: 
-                max_ones = current_ones 
-                row_index = i # return the index of that specific row 
-
-        return [row_index, max_ones]
-
-mat = [[0,0],[1,1],[0,0]]
-solution = Solution()
-print(solution.rowAndMaximumOnes(mat))
-
-            
+        one_index = 0
+        for i in range(len(mat)): 
+            current_sum = sum(mat[i])
+            if current_sum > max_ones: 
+                max_ones = current_sum
+                one_index = i
+        return [one_index, max_ones]

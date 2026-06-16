c7af6102a373f9fa1532fa39f050e883af34e2f9
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-06-16 06:06:39 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (87.40%) - LeetSync

--- 1677-matrix-diagonal-sum/matrix-diagonal-sum.py
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.py
@@ -1,20 +1,9 @@
-class Solution:
+class Solution: 
     def diagonalSum(self, mat): 
         n = len(mat)
-        total_sum = 0
-
-        for i in range(n):
-            total_sum += mat[i][i] # to get the normal diagonal 
-            if i != n - 1 - i:
-                total_sum += mat[i][n - 1 - i] # to get the reverse or the opposite diagonal 
-                # write the values in pen and paper to undersnad better 
-
-        return total_sum
-
-mat = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9]
-    ]
-solution = Solution()
-print(solution.diagonalSum(mat))
+        total = 0 
+        for i in range(n): 
+            total += mat[i][i]
+            if i != n - 1 - i: 
+                total += mat[i][n - 1 - i]
+        return total

6e3c649d187e0b460581069e2b6b10438cf83f4c
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-06-17 20:14:14 +0530

    Time: 3 ms (52.82%) | Memory: 27.7 MB (79.68%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -1,39 +1,23 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution:
-    def reorderList(self, head: Optional[ListNode]) -> None:
-        """
-        Do not return anything, modify head in-place instead.
-        """
+class Solution: 
+    def reorderList(self, head): 
         if not head or not head.next: 
             return 
-
-        slow = head
-        fast = head.next # here we want the slow pointer to stay at the previous index of the mid thats why we have forwarded the fast by one element 
+        slow = head 
+        fast = head.next # next val 
         while fast and fast.next: 
             slow = slow.next
-            fast = fast.next.next # find the mid position
-
-        second = slow.next
-        prev = slow.next = None # here we are partitioning the list into two 
-        # 1st part the header is head and for the 2nd part the header is None or prev
+            fast = fast.next.next 
+        second = slow.next # placeholder
+        prev = slow.next = None 
         while second: 
-            temp = second.next
-            second.next = prev
+            temp = second.next 
+            second.next = prev 
             prev = second
-            second = temp # reverse the 2nd part 
-
-        # now merge the 2nd part
+            second = temp
         first = head 
-        second = prev # head pointers for both of the lists 
+        second = prev
         while second: 
-            temp1, temp2 = first.next, second.next # pointers 
-            first.next = second # 2nd element 
-            second.next = temp1 # 3rd element
+            temp1, temp2 = first.next, second.next 
+            first.next = second 
+            second.next = temp1
             first, second = temp1, temp2 # increment 
-
-
-        

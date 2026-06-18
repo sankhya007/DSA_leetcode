26ab4998420f4f03b15d086bf27cfa16eb7a58d9
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-06-18 15:50:31 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (72.85%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -1,21 +1,15 @@
-class Solution:
-    def evalRPN(self, tokens: List[str]) -> int:
+class Solution: 
+    def evalRPN(self, tokens): 
         stack = []
         operators = "+-*/"
-
         for token in tokens: 
             if token in operators: 
-                b = stack.pop()
-                a = stack.pop()
-
+                b = stack.pop() # right
+                a = stack.pop() # left (position of the right and left matters)
                 if token == '+': stack.append(a + b)
                 elif token == '-': stack.append(a - b)
                 elif token == '*': stack.append(a * b)
-                elif token == '/': stack.append(int(a / b)) # trunkate towards zero 
-                # you can not use "//" / round down function because 5 // 2 = 3 but int(5/2) = 2 closer to zero
-
+                elif token == '/': stack.append(int(a / b))
             else: 
-                stack.append(int(token))
-
+                stack.append(int(token)) # if an value - add in stack as int
         return stack[0]
-        # after doing all that the stack is going to contain only one value but of we use return stack we are going to get a list in return like "[9]" and that is not what we want, we want a value like 9 that's why we are calling an index 

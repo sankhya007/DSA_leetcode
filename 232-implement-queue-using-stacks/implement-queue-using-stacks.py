bcd96f3a731ba484eef1ed680175e2fcd1a5e7e2
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-06-19 11:34:43 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (85.86%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.py
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.py
@@ -1,29 +1,16 @@
-class MyQueue:
-
-    def __init__(self):
+class MyQueue: 
+    def __init__(self): 
         self.in_stack = []
-        self.out_stack = [] # initializing both of the stacks 
-
-    def push(self, x: int) -> None:
-        self.in_stack.append(x) # only the  instack will append values 
-
-    def pop(self) -> int:
-        self.peek() # here to make the stacks act like queue 
-        return self.out_stack.pop() # just pop the top most eleemnt after doing the peek 
-
-    def peek(self) -> int:
-        if not self.out_stack: 
-            while self.in_stack: 
-                self.out_stack.append(self.in_stack.pop())
-        return self.out_stack[-1] # litrallry go through it in the opposite direction and pop out the exact opposite number of the stack you 1st had, coz you fkn reversed it
-
-    def empty(self) -> bool:
-        return not self.in_stack and not self.out_stack # if everything is empty return true 
-
-
-# Your MyQueue object will be instantiated and called as such:
-# obj = MyQueue()
-# obj.push(x)
-# param_2 = obj.pop()
-# param_3 = obj.peek()
-# param_4 = obj.empty()
+        self.out_stack = [] # initialize
+    def push(self, x): 
+        self.in_stack.append(x) 
+    def pop(self): 
+        self.peek()
+        return self.out_stack.pop()
+    def peek(self): 
+        if not self.out_stack: # is no value in out 
+            while self.in_stack: # and value in "in"
+                self.out_stack.append(self.in_stack.pop()) # reverse add from in to out 
+        return self.out_stack[-1] # return last element - actually 1st 
+    def empty(self): 
+        return not self.in_stack and not self.out_stack

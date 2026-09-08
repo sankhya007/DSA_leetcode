a92a820aef85e3d9e1a871a215325431d62f481b
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-09-08 16:42:21 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (68.21%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,8 +1,13 @@
-class Solution(): 
+class Solution: 
     def twoSum(self, nums, target): 
         num_set = {}
+
         for i, num in enumerate(nums): 
             complement = target - num
+
             if complement in num_set: 
                 return [num_set[complement], i]
-            num_set[num] = i
+
+            num_set[num] = i 
+
+    

aefd9d4eaa19a76dc3d3e9ea5d0051a0659a337d
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-09-08 18:13:09 +0530

    Time: 3 ms (86.50%) | Memory: 13.8 MB (17.78%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,5 +1,13 @@
-class Solution:
-    def majorityElement(self, nums): 
-        n = len(nums)//2
-        nums.sort()
-        return nums[n]
+class Solution(): 
+	def majorityElement(self, nums): 
+		n = len(nums)//2
+		nums.sort()
+		return nums[n]
+
+# index:  0  1  2  3  4  5  6
+#         ↓  ↓  ↓  ↓  ↓  ↓  ↓
+# nums:  [1, 1, 1, 2, 2, 2, 2]
+#                   ↑
+#                 nums[3]
+
+# this is why the nums n work 

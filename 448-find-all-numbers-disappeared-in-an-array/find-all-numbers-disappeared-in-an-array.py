dcf36d60f06e670a1c306989a2340ea8a944fe43
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-09-08 23:16:59 +0530

    Time: 34 ms (62.32%) | Memory: 26.6 MB (99.61%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.py
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.py
@@ -1,13 +1,16 @@
-class Solution:
-    def findDisappearedNumbers(self, nums): 
-        n = len(nums) # to see how manu characters we have in the list 
-        num_set = set(nums) # get rid of all the duplicates in the array 
-        missing_numbers = [] # storage for missing number/s
-        for i in range(1, n+1): # run the loop till one extra index
-            if i not in num_set: 
-                missing_numbers.append(i)
-        return missing_numbers
+class Solution: 
+	def findDisappearedNumbers(self, nums): 
 
-nums = [4,3,2,7,8,2,3,1]
-solution = Solution()
-print(solution.findDisappearedNumbers(nums))
+		# length and storage
+		n = len(nums)
+		num_set = set(nums)
+		missing_number = []
+
+		# only works because the missing ones are duplicates of others
+		for num in range(1, n+1): 
+			# if not found in num_set then append
+			if num not in num_set: 
+				missing_number.append(num)
+
+		# display 
+		return missing_number

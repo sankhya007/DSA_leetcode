cb9106deca63bf54add0aca886066bf5fe12cd82
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-09-08 20:17:46 +0530

    Time: 26 ms (61.92%) | Memory: 25.8 MB (92.17%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,9 +1,15 @@
-class Solution(): 
-    def containsDuplicate(self, nums): 
-        num_set = set()
-        for num in nums: 
-            if num in num_set: 
-                return True
-            else: 
-                num_set.add(num)
-        return False
+class Solution: 
+	def containsDuplicate(self, nums): 
+		# create storage
+		num_set = set()
+
+		for num in nums: 
+			# if exists return True
+			if num in num_set: 
+				return True
+			# if not then add to set
+			else: 
+				num_set.add(num)
+
+		# not exists so return False
+		return False

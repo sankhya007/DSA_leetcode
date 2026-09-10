01f9dc722c19b6eb553acf130c48835e81966e9c
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-09-10 20:18:08 +0530

    Time: 6 ms (91.39%) | Memory: 13.8 MB (99.59%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -1,43 +1,45 @@
-# class Solution(): 
-# 	def sortedSquares(self, nums): 
-# 		for i, num in enumerate(nums):
-# 			nums[i] = num*num
-# 		nums.sort()
-# 		return nums
-
-# time complexity O(n log n) ^
-
+# time complexity O(n log n)
 
 class Solution(): 
 	def sortedSquares(self, nums): 
+		for i, num in enumerate(nums):
+			nums[i] = num ** 2
+		nums.sort()
+		return nums
 
-		# pointers
-		left = 0 
-		right = len(nums) - 1
 
-		# storage
-		result = [0] * len(nums)
 
-		# start from end
-		position = len(nums) - 1
-
-		# initial loop
-		while left <= right: 
-
-			# absolute value because when sq "+ve" value or "-ve" value doesn't really matter
-			# left > right, coz coming from backwards
-			if abs(nums[left]) > abs(nums[right]): 
-				result[position] = nums[left] ** 2
-				left += 1
-
-			# abs(nums[right]) > abs(nums[left])
-			else: 
-				result[position] = nums[right] ** 2 
-				right -= 1
-
-			# next variable
-			position -= 1
-
-		return result
 
 # time complexity O(n)
+
+# class Solution(): 
+# 	def sortedSquares(self, nums): 
+
+# 		# pointers
+# 		left = 0 
+# 		right = len(nums) - 1
+
+# 		# storage
+# 		result = [0] * len(nums)
+
+# 		# start from end
+# 		position = len(nums) - 1
+
+# 		# initial loop
+# 		while left <= right: 
+
+# 			# absolute value because when sq "+ve" value or "-ve" value doesn't really matter
+# 			# left > right, coz coming from backwards
+# 			if abs(nums[left]) > abs(nums[right]): 
+# 				result[position] = nums[left] ** 2
+# 				left += 1
+
+# 			# abs(nums[right]) > abs(nums[left])
+# 			else: 
+# 				result[position] = nums[right] ** 2 
+# 				right -= 1
+
+# 			# next variable
+# 			position -= 1
+
+# 		return result

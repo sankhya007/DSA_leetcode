4f15f9117a0ba7836fca95eb916bfd8f56e57889
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-09-10 18:50:01 +0530

    Time: 6 ms (56.69%) | Memory: 14.5 MB (5.97%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,13 +1,35 @@
-class Solution: 
-    def twoSum(self, numbers, target): 
-        left = 0 
-        right = len(numbers) - 1
-        while left < right: 
-            current_sum = numbers[left] + numbers[right]
-            if current_sum == target: 
-                return [left+1, right+1]
-            elif current_sum < target: 
-                left += 1
-            elif current_sum > target: 
-                right -= 1
-        return None
+# two sum, input array sorted - leetcode 
+
+class Solution(): 
+	def twoSum(self, numbers, target): 
+
+		# pointers 
+		left = 0 
+		right = len(numbers) - 1
+
+		# initial condition
+		while left < right: 
+
+			# current_sum calculation 
+			current_sum = numbers[left] + numbers[right]
+
+			# if sum same as target, return position
+			if current_sum == target: 
+				return [left + 1, right + 1] 
+
+			# when sum smaller, move further up in array
+			elif current_sum < target: 
+				left += 1 
+
+			# when sum larger, move further down in array
+			elif current_sum > target: 
+				right -= 1
+
+		return None
+
+
+numbers = [2,7,11,15]
+target = 9 
+
+sol = Solution() 
+print(sol.twoSum(numbers, target))

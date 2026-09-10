91e4098b8f70fdeb72f225f9abd347812f45f523
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-09-10 20:17:12 +0530

    Time: 19 ms (17.96%) | Memory: 14.2 MB (73.92%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -1,9 +1,43 @@
+# class Solution(): 
+# 	def sortedSquares(self, nums): 
+# 		for i, num in enumerate(nums):
+# 			nums[i] = num*num
+# 		nums.sort()
+# 		return nums
+
+# time complexity O(n log n) ^
+
+
 class Solution(): 
 	def sortedSquares(self, nums): 
 
-		for i, num in enumerate(nums):
-			nums[i] = num*num
+		# pointers
+		left = 0 
+		right = len(nums) - 1
 
-		nums.sort()
+		# storage
+		result = [0] * len(nums)
 
-		return nums
+		# start from end
+		position = len(nums) - 1
+
+		# initial loop
+		while left <= right: 
+
+			# absolute value because when sq "+ve" value or "-ve" value doesn't really matter
+			# left > right, coz coming from backwards
+			if abs(nums[left]) > abs(nums[right]): 
+				result[position] = nums[left] ** 2
+				left += 1
+
+			# abs(nums[right]) > abs(nums[left])
+			else: 
+				result[position] = nums[right] ** 2 
+				right -= 1
+
+			# next variable
+			position -= 1
+
+		return result
+
+# time complexity O(n)

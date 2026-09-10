a44baf9d7f4b3e29faa5155ff8f7fcea04660ae9
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-09-10 19:13:36 +0530

    Time: 106 ms (79.67%) | Memory: 20.7 MB (25.35%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,14 +1,33 @@
-class Solution: 
-    def maxArea(self, heights): 
-        left = 0 
-        right = len(heights) - 1
-        max_area = 0
-        while left < right: 
-            width = right - left
-            current_area = min(heights[left], heights[right]) * width
-            max_area = max(max_area, current_area)
-            if heights[left] < heights[right]: 
-                left += 1
-            else: # right < left 
-                right -= 1
-        return max_area
+# container with most water - leetcode
+
+class Solution(): 
+	def maxArea(self, heights): 
+
+		# pointers
+		left = 0 
+		right = len(heights) - 1
+
+		# storage
+		max_area = 0 
+
+		# initial loop 
+		while left < right: 
+
+			# container bottom width 
+			containter_width = right - left
+
+			# will be changing for each columns till none left
+			variable_area = min(heights[left], heights[right]) * containter_width
+
+			# will capture only the max 
+			max_area = max(max_area, variable_area)
+
+			# trying to match up the smaller left with bigger right, by left increment
+			if heights[left] < heights[right]: 
+				left += 1 
+
+			# right increment
+			else: # right < left
+				right -= 1
+
+		return max_area

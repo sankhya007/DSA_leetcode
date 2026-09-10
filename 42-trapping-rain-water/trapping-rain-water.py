435038820e6b79998588da0c37008c62a4d041f7
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-09-10 16:00:13 +0530

    Time: 3 ms (98.18%) | Memory: 13.4 MB (59.48%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,24 +1,61 @@
-class Solution: 
-    def trap(self, height): 
-        if not height: 
-            return 0
-        left = 0
-        right = len(height) - 1
-        max_left = 0
-        max_right = 0
-        trapped_water = 0
-        while left < right: 
-            if height[left] < height[right]: 
-                if height[left] >= max_left: 
-                    max_left = height[left]
-                else: 
-                    trapped_water += max_left - height[left]
-                left += 1
-            else: 
-                if height[right] >= max_right: 
-                    max_right = height[right]
-                else: 
-                    trapped_water += max_right - height[right]
-                right -= 1
-        return trapped_water
-                
+# trapped rain water - leetcode 
+
+class Solution():
+	def trap(self, height): 
+
+		# in case list empty
+		if not height: 
+			return 0 
+
+		# two pointers
+		left = 0 
+		right = len(height) - 1
+
+		# max pointers 
+		max_left = 0 
+		max_right = 0 
+
+		# storage 
+		trapped_water = 0 
+
+		# starting sequence 
+		while left < right: 
+
+			# calculation sequence if the left height bigger top code block will run, else bottom block 
+
+			# top block
+			if height[left] < height[right]: 
+
+				# if left column bigger than initial left, store 
+				if height[left] >= max_left: 
+					max_left = height[left]
+
+				# total amount of water
+				else: 
+					trapped_water += max_left - height[left]
+
+				# increment
+				left += 1 
+
+			# bottom block
+			else: 
+
+				# if right column bigger than initial right, store
+				if height[right] >= max_right: 
+					max_right = height[right]
+
+				# total water captured 
+				else: 
+					trapped_water += max_right - height[right]
+
+				# increment 
+				right -= 1 
+
+		return trapped_water
+
+
+heights = [0,1,0,2,1,0,1,3,2,1,2,1]
+
+solution = Solution()
+print(solution.trap(heights))
+

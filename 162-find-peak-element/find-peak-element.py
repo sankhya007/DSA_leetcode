d3f5a24ac558a9f405041624936ca5715c284513
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-09-12 19:44:58 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (54.26%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -1,9 +1,29 @@
-class Solution(object):
-    def findPeakElement(self, nums):
-        peek = max(nums)
+# class Solution(object):
+#     def findPeakElement(self, nums):
+#         peek = max(nums)
 
-        for i, num in enumerate(nums): 
-            if num == peek: 
-                return i
+#         for i, num in enumerate(nums): 
+#             if num == peek: 
+#                 return i
 
-                
+
+# fnds the peek without scanning the entire array
+class Solution(): 
+    def findPeakElement(self, nums): 
+
+        # pointers
+        left = 0 
+        right = len(nums) - 1
+
+        # initial loop
+        while left < right: 
+            mid = (left + right) // 2 
+
+            # if the mid is bigger than the mid + 1 then the peek should be at the left side of the array, because if the next number is small that means the number is gradually decreasing
+            if nums[mid] > nums[mid + 1]: 
+                right = mid
+
+            else: 
+                left = mid + 1 
+
+        return left

ad1ccd41e812a5756126318e1eed8f4727918fc3
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-09-15 09:48:38 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (68.61%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,18 +1,45 @@
-class Solution: 
-    def searchMatrix(self, matrix, target): 
-        if not matrix or not matrix[0]: 
-            return False
-        rows = len(matrix)
-        cols = len(matrix[0])
-        left = 0
-        right = rows * cols - 1
-        while left <= right: 
-            mid = left + (right - left) // 2
-            mid_value = matrix[mid // cols][mid % cols]
-            if mid_value == target: 
-                return True
-            elif mid_value < target: 
-                left = mid + 1
-            else: 
-                right = mid - 1
-        return False
+# search in a 2d matrix 
+
+class Solution(): 
+	def searchMatrix(self, matrix, target): 
+
+		# check if the matrix is empty
+		if not matrix and not matrix[0]: 
+			return False 
+
+		# count rows and cols
+		rows = len(matrix)
+		cols = len(matrix[0])
+
+		# pointers
+		left = 0 
+		right = rows * cols - 1 # last element rows*cols
+
+		# initial loop 
+		while left <= right: 
+
+			# mid number
+			mid = left + (right - left) // 2 
+			# value col
+			mid_value = matrix[mid // cols][mid % cols]
+
+			# if same return
+			if mid_value == target: 
+				return True
+
+			# if value bigger than mid, leave out left part 
+			elif mid_value < target: 
+				left = mid + 1 
+
+			# if value smaller, leave out right part
+			else: 
+				right = mid - 1 
+
+		return False
+
+
+matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
+target = 3
+
+sol = Solution()
+print(sol.searchMatrix(matrix, target))

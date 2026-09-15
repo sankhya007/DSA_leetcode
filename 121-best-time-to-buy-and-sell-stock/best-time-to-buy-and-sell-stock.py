f8b69611be3eedc4de233de4aae7bd90957217c4
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-09-15 11:16:46 +0530

    Time: 38 ms (78.51%) | Memory: 18.9 MB (76.12%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,10 +1,24 @@
-class Solution: 
+# best time to buy and sell stock 
+
+class Solution(): 
     def maxProfit(self, prices): 
-        max_profit = 0
+
+        max_profit = 0 
         min_price = float("inf")
+        # cant be bigger than this
+
         for price in prices: 
             if price < min_price: 
                 min_price = price
+                # min price is the lowest we have seen
+
+            # will find the max difference
             elif price - min_price > max_profit: 
-                max_profit = price - min_price 
+                max_profit = price - min_price
+
         return max_profit
+
+prices = [7,1,5,3,6,4]
+
+sol = Solution()
+print(sol.maxProfit(prices))

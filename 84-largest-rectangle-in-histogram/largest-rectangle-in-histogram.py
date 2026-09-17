3c97ad6a937682cc502cf6f90518e8ded6abda2f
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-09-17 11:23:41 +0530

    Time: 106 ms (91.01%) | Memory: 31.4 MB (48.17%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -1,12 +1,30 @@
 class Solution:
     def largestRectangleArea(self, heights):
+
+        # storage
         stack = []
+
         max_area = 0 
+        # so everything process
         heights.append(0)
+
         for i, h in enumerate(heights): 
-            while stack and heights[stack[-1]] > h: # the one we are looking at is smaller than the last one 
-                height = heights[stack.pop()] 
-                width = i if not stack else i - stack[-1] - 1 #right - left - 1
+
+            # till increasing we keep going 
+            # when hit a smaller one we stop
+            while stack and heights[stack[-1]] > h: 
+
+                # assign the height
+                height = heights[stack.pop()]
+
+                # when stack has no value
+                # else, current - last height - left boundary
+                # not heights[stack] coz we are counting the index and stack only have the indexs and heights have the values
+                width = i if not stack else i - stack[-1] -1 
+
                 max_area = max(max_area, height * width)
+
+            # only append the index when in increasing order
             stack.append(i)
+
         return max_area

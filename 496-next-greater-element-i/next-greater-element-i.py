b33704d10e1364030f6cddc4f416467adb0a6658
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-09-17 12:18:56 +0530

    Time: 1 ms (48.90%) | Memory: 19.5 MB (30.23%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,12 +1,29 @@
-class Solution: 
+class Solution(): 
     def nextGreaterElement(self, nums1, nums2): 
+
+        # storage
         stack = []
+        # will store enumerate values
         next_greater = {}
-        for num in nums2: 
+
+        # loo through the main list, nums1 is the subset of nums2 
+        for num in nums2:  
+
+            # stack has value, current value bigger than stack
             while stack and num > stack[-1]: 
-                prev = stack.pop()
+
+                # not stack[-1], because that value stays in the stack
+                prev = stack.pop() 
+
+                # next_greater element in the index position of the previous element
                 next_greater[prev] = num
+
+            # if the number is not 
             stack.append(num)
-            for num in stack: 
-                next_greater[num] = -1
+
+        # make all the left over values in stack, not in next_greater store as -1
+        while stack: 
+            next_greater[stack.pop()] = -1
+            
+        # loop through all of the elements in next greater and give out values stored in the next_greater
         return [next_greater[num] for num in nums1]

7479492df714136c7611f874d59cc5754ad36a08
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-09-17 16:49:04 +0530

    Time: 88 ms (11.56%) | Memory: 24 MB (70.65%) - LeetSync

--- 937-online-stock-span/online-stock-span.py
+++ 937-online-stock-span/online-stock-span.py
@@ -1,19 +1,19 @@
-class StockSpanner:
-
-    def __init__(self):
+class StockSpanner(): 
+    
+    def __init__(self): 
         self.stack = []
 
-    def next(self, price: int) -> int:
+    def next(self, price): 
         span = 1 
 
+        # stack has value and the span together (x, y)
         while self.stack and self.stack[-1][0] <= price: 
+
+            # if the price now is equal of bigger than the 1st value of the stack, the span append with the selected value's span 
             span += self.stack.pop()[1]
 
+        # push today as a tuple so that future days can absorb span the same way
         self.stack.append((price, span))
 
         return span
 
-
-# Your StockSpanner object will be instantiated and called as such:
-# obj = StockSpanner()
-# param_1 = obj.next(price)

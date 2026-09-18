9ff0d780a0ab66b1632c787e0b79276c156020a3
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-09-18 09:39:50 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (96.17%) - LeetSync

--- 394-decode-string/decode-string.py
+++ 394-decode-string/decode-string.py
@@ -1,6 +1,5 @@
 class Solution(): 
     def decodeString(self, s): 
-
         stack = []
         current_string = ""
         current_number = 0 
@@ -8,17 +7,28 @@ class Solution():
         for c in s: 
 
             if c.isdigit(): 
+                # multiply by 10 step added to then if the number is in double digits like "32, 41" we can have a int form of that, and for single digits it is getting multiplied with 0 so don't cause a difference
                 current_number = current_number * 10 + int(c)
 
             elif c == '[': 
+                # stack append and variable reset
                 stack.append((current_string, current_number))
                 current_string, current_number = "", 0
 
             elif c == ']': 
+                # assign values
                 prev_string, num = stack.pop()
-                current_string = prev_string + current_string * num 
 
+                # the number will be in front so that's why we are multiplying the current_string with the previous number
+                current_string = prev_string + current_string * num
+
+            # if we have more than one string alongside of each other
             else: 
                 current_string += c 
 
         return current_string
+
+
+
+
+

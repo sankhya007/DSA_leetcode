b168f5b4282d8ac458afac33ae5ba38981c1f2e0
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-09-18 09:39:27 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (96.17%) - LeetSync

--- 394-decode-string/decode-string.py
+++ 394-decode-string/decode-string.py
@@ -1,17 +1,24 @@
-class Solution: 
+class Solution(): 
     def decodeString(self, s): 
+
         stack = []
         current_string = ""
         current_number = 0 
+
         for c in s: 
+
             if c.isdigit(): 
-                current_number = current_number * 10 + int(c) # add space "0"
+                current_number = current_number * 10 + int(c)
+
             elif c == '[': 
-                stack.append((current_string, current_number)) # add in stack
-                current_string, current_number = "", 0 # reset back 
+                stack.append((current_string, current_number))
+                current_string, current_number = "", 0
+
             elif c == ']': 
-                prev_string, num = stack.pop() # assign value
-                current_string = prev_string + current_string * num # multiply with current
+                prev_string, num = stack.pop()
+                current_string = prev_string + current_string * num 
+
             else: 
-                current_string += c # is two strings together
-        return current_string 
+                current_string += c 
+
+        return current_string

49a2ce9b3004e55a36bdacced73f4a9cd02d1406
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-09-18 18:36:23 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (25.75%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -6,18 +6,30 @@
 class Solution:
     def removeNthFromEnd(self, head: ListNode | None, n: int) -> ListNode | None:
         
+       # make a dummy node before the head, call it head
         dummy = ListNode(0, head)
 
+        # point both of the pointers to the dummy 
         slow = dummy 
         fast = dummy 
 
+        # for however many number we need of the end let the fast pointer go that many times
         for _ in range(n): 
             fast = fast.next 
 
+        # then run the slow and the fast pointer together
         while fast.next: 
-            fast = fast.next 
-            slow = slow.next 
+            slow = slow.next
+            fast = fast.next
+        # after this the slow pointer will be at the intended delete position
 
+        # delete the position
         slow.next = slow.next.next
 
+        # return dummy.next because the code might have caused the head to be deleted, but the dummy.next element will always be there
         return dummy.next
+
+
+
+
+

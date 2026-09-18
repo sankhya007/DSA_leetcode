bc46740cb1ad6e1e8ac47091c397e7bd21f642cb
Sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-09-18 10:03:24 +0530

    Time: 15 ms (95.15%) | Memory: 20 MB (66.28%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.py
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.py
@@ -4,6 +4,9 @@ class Solution:
         stack = []
 
         for c in s: 
+
+            # if something in stack that matches the current element, pop that element from stack
+            # if it does not match add that element in stack 
             if stack and stack[-1] == c: 
                 stack.pop()
             else: 
